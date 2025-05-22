42bc043c05d6126585ae860ad7c6e0b0b509adc9
Varun Lohade <50517140+varunlohade@users.noreply.github.com>
2025-05-22 22:26:06 +0530

    websocket changed

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export default {    
-    websocketUrl: 'wss://wsserver-production-9341.up.railway.app'
+    websocketUrl: 'wss://wsserver-production-96ea.up.railway.app'
     // websocketUrl: "ws://localhost:5001"
 }
